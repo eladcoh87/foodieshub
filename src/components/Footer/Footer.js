bc973c9a6a88582e7fd51ba1ad915f2bc7ef6dde
EladC <elad.amdocs@gmail.com>
2023-01-09 14:39:06 +0200

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,30 +16,30 @@ const Footer = () => {
 <Grid className='gridContainer' container spacing={2}>
 
 <Grid className='gridItem' container item xs={4}  justifyContent="center"
-  alignItems="center"   direction="column"
+  alignItems="center"   direction="column" justifyItems="center"
   >
 
 
-<h3>HEADLINE</h3>
+<h3>Contact</h3>
 <ul>
-<li> <a href="#">link</a> </li>
-<li> <a href="#">link</a> </li>
-<li> <a href="#">link</a> </li>
+<li> <a href="#">About Us</a> </li>
+<li> <a href="#">Editorial Process</a> </li>
+<li> <a href="#">Privacy Policy</a> </li>
 
 </ul>
 
 </Grid>
 
 <Grid container item xs={4}  justifyContent="center"
-  alignItems="center"   direction="column"
+  alignItems="center" justifyItems="center"   direction="column"
   >
 
 
-<h3>HEADLINE</h3>
+<h3>Terms of Use</h3>
 <ul>
-<li> <a href="#">link</a> </li>
-<li> <a href="#">link</a> </li>
-<li> <a href="#">link</a> </li>
+<li> <a href="#">Content Licensing</a></li>
+<li> <a href="#">Anti-Racism Pledge</a></li>
+<li> <a href="#">Product Vetting</a></li>
 
 </ul>
 
@@ -47,15 +47,15 @@ const Footer = () => {
 
 
 <Grid container item xs={4}  justifyContent="center"
-  alignItems="center"   direction="column"
+  alignItems="center" justifyItems="center"  direction="column"
   >
 
 
-<h3>HEADLINE</h3>
+<h3>ABOUT US</h3>
 <ul>
-  <li> <a href="#">link</a> </li>
-  <li> <a href="#">link</a> </li>
-  <li> <a href="#">link</a> </li>
+<li> <a href="#">Content Licensing</a></li>
+<li> <a href="#">Anti-Racism Pledge</a></li>
+<li> <a href="#">Product Vetting</a></li>
 
 </ul>
 
